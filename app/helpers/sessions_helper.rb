f92958cf92bafdf15e804de1c978b9212fc9a681
rhico-sunAsterisk <rhico.abueme@sun-asterisk.com>
2020-06-28 15:40:14 +0800

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,28 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+    
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.encrypted[:user_id])
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
 
+    def current_user?(user)
+        user && user == current_user
+    end
+    
     def logged_in?
         !current_user.nil?
     end
@@ -17,5 +33,9 @@ module SessionsHelper
     def log_out
         reset_session
         @current_user = nil
+    end
+
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 end
