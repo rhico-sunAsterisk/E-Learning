b83369158575f07aa584221f59105c1233669d6d
rhico-sunAsterisk <rhico.abueme@sun-asterisk.com>
2020-07-09 11:32:22 +0800

    follow users
    follow users
    
    follow users
    
    Follow users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_103100) do
+ActiveRecord::Schema.define(version: 2020_07_09_021649) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
