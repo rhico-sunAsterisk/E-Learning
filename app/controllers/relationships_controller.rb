0da1f984d142a3f31649d8b514ed6dcfe37c0bbb
rhico-sunAsterisk <rhico.abueme@sun-asterisk.com>
2020-07-09 22:56:22 +0800

    Fixed the spacing. Deleted unimportant files.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,15 @@
 class RelationshipsController < ApplicationController
-    before_action :logged_in?
+  before_action :logged_in?
     
-    def create
-        user = User.find(params[:followed_id])
-        current_user.follow(user)
-        redirect_to user
-    end
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
+  end
         
-    def destroy
-        user = Relationship.find(params[:id]).followed
-        current_user.unfollow(user)
-        redirect_to user
-    end
+  def destroy
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
+  end
 end
